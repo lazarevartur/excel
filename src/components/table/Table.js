610b77f96511b99a9fa2d1c2cebd6f64004ccdaf
Arthur <gracian950@gmail.com>
2020-10-20 02:44:18 +0300

    finish base logick

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -8,10 +8,11 @@ import {$} from '@core/dom';
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown']
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     });
     this.prepare()
   }
@@ -20,8 +21,17 @@ export class Table extends ExcelComponent {
   }
   init() {
     super.init();
+    this.$on('formula:input', (text) => {
+      console.log(text)
+      this.selection.current.text(text)
+    })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
     const $cell = this.$root.find(['[data-id="0:0"]'])
     this.selection.select($cell)
+    this.$emit('table:input', $cell.text())
   }
 
   toHtml() {
@@ -41,6 +51,7 @@ export class Table extends ExcelComponent {
         this.selection.groupSelect($cells)
       } else {
         this.selection.select($cell)
+        this.$emit('table:select', $cell)
       }
     }
   }
@@ -55,11 +66,16 @@ export class Table extends ExcelComponent {
     ]
     const {key} = event
     if (keys.includes(key)) {
+      event.preventDefault()
       const current = this.selection.current.id(true)
       const id = nextCell(key, current)
-      event.preventDefault()
-      this.selection.select(this.$root.find(`[data-id="${id}"]`))
+      const $next = this.$root.find(`[data-id="${id}"]`)
+      this.selection.select($next)
+      this.$emit('table:select', $next)
     }
+  }
+  onInput(event) {
+    this.$emit('table:input', $(event.target).text())
   }
 }
 function nextCell(key, current) {
